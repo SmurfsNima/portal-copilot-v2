0d45e3feada4bde5c4f26ec400b0586999f22b7b
amir <amirdehghani137722@gmail.com>
2024-09-04 09:34:12 +0330

    update login image

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -60,7 +60,7 @@ const Login = () => {
     // </div>
 <div className="bg-black-primary flex justify-center items-center h-screen">
 <div className="w-1/2 h-screen hidden lg:block">
-  <img src="https://placehold.co/800x/121212/ffffff.png?text=Your+Image&font=Montserrat" alt="Placeholder Image" className="object-cover w-full h-full" />
+  <img src={'./images/loginPhoto.jpg'} alt="Placeholder Image" className="object-cover w-full h-full" />
 </div>
 <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
   <h1 className="text-2xl text-primary-text font-semibold mb-4">Login</h1>
