e3da51f08c3dc15c47ac0a00b8cf89c763e92353
amir <amirdehghani137722@gmail.com>
2024-09-05 12:56:14 +0330

    fix build issue

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Api from "./api";
-import allBiomarkers from './--moch--/data/Allbiomarkers.json';
+// import allBiomarkers from './--moch--/data/Allbiomarkers.json';
 import AllBloodtests from './--moch--/data/AllBloodtests.json'
 class Application extends Api {
   static getPatients() {
