bc63136542d78322ad6f931899c94ee710403bbc
amir <amirdehghani137722@gmail.com>
2024-10-03 13:10:55 +0330

    hidden modals

--- src/pages/aiStudio/GenerateWithAiModal.tsx
+++ src/pages/aiStudio/GenerateWithAiModal.tsx
@@ -10,11 +10,11 @@ const GenerateWithAiModal:React.FC<GenerateWithAiModalProps> = ({
 }) => {
     return (
         <>
-            <div ref={refEl} className="w-[205px] h-[260px] overflow-auto px-3 py-2 rounded-[6px] bg-[#272727] border border-[#383838]">
+            <div ref={refEl} className="w-[205px] h-[190px] overflow-auto px-3 py-2 rounded-[6px] bg-[#272727] border border-[#383838]">
                 <div className="bg-black-primary border border-main-border rounded-md -ml-1 ">
                     <input className="bg-transparent outline-none text-[10px] pl-2  py-2 opacity-80 " type="text" placeholder="Ask AI to..." />
                 </div>
-                <div onClick={() => {
+                {/* <div onClick={() => {
                     onSuccess("Improve Writing")
                 }} className="text-[10px] text-[#FFFFFFDE] h-[34px] flex justify-start items-center border-b cursor-pointer border-b-[#383838]">Improve Writing</div>
                 <div onClick={() => {
@@ -22,7 +22,7 @@ const GenerateWithAiModal:React.FC<GenerateWithAiModalProps> = ({
                 }} className="text-[10px] text-[#FFFFFFDE] h-[34px] flex justify-start items-center border-b cursor-pointer border-b-[#383838]">Fix Spelling & Grammar</div>
                 <div onClick={() => {
                     onSuccess("Translate")
-                }} className="text-[10px] text-[#FFFFFFDE] h-[34px] flex justify-start items-center border-b cursor-pointer border-b-[#383838]">Translate</div>
+                }} className="text-[10px] text-[#FFFFFFDE] h-[34px] flex justify-start items-center border-b cursor-pointer border-b-[#383838]">Translate</div> */}
                 <div onClick={() => {
                     onSuccess("Make Longer")
                 }} className="text-[10px] text-[#FFFFFFDE] h-[34px] flex justify-start items-center border-b cursor-pointer border-b-[#383838]">Make Longer</div>
