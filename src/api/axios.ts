a04f09065c798b718d4248a6681a0920d29f4c80
amir <amirdehghani137722@gmail.com>
2024-10-05 13:28:26 +0330

    fix when token is invalid

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,7 +6,7 @@ axios.interceptors.response.use((response) => {
     if(response.status === 200) {
          toast.dismiss()
     }
-    if(response.status ==401 || response.status ==498 || response.data.detail == 'Unauthorized - User Not Found'||response.data.detail == 'Invalid or logged out token'||response.data.detail == 'Missing Authorization Header'||response.data.detail == 'Unauthorized - User does not match token' || response.data.detail == 'Signature has expired'){
+    if(response.status ==401 || response.data.detail =='Invalid token.'){
         localStorage.clear()
         window.location.reload(); 
     }    
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
 import AppContextProvider from './store/app.tsx'
+import './api/axios.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
